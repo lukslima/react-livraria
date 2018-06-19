968ac1ca80fb058c20e59df16a4f1d6c119146e8
Lucas Ferreira de Lima <lucas.ferreira@detran.ce.gov.br>
2018-06-19 11:37:36 -0300

    inputCustomizado e atualização de lista apor salvar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './css/pure-min.css';
 import './css/side-menu.css';
 import $ from 'jquery';
+import InputCustomizado from './componentes/InputCustomizado';
 
 class App extends Component {
 
@@ -20,7 +21,6 @@ class App extends Component {
       url:"http://localhost:9091/api/autores",
       dataType: 'json',
       success: function(resposta){
-        console.log("chegou a resposta");
         this.setState({lista : resposta});
       }.bind(this)
     });
@@ -48,9 +48,8 @@ class App extends Component {
       contentType:"application/json",
       data: JSON.stringify({nome: this.state.nome, email: this.state.email, senha: this.state.senha}),
       success: function(resposta) {
-        console.log("Resposta Sucesso!");
-        console.log(resposta);
-      },
+        this.setState({lista:resposta});
+      }.bind(this),
       error: function(resposta) {
         console.log("Deu error");
         console.log(resposta);
@@ -88,18 +87,10 @@ class App extends Component {
             <div className="content" id="content">
               <div className="pure-form pure-form-aligned">
                 <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm}>
-                  <div className="pure-control-group">
-                    <label htmlFor="nome">Nome</label> 
-                    <input id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome} />                  
-                  </div>
-                  <div className="pure-control-group">
-                    <label htmlFor="email">Email</label> 
-                    <input id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail} />                  
-                  </div>
-                  <div className="pure-control-group">
-                    <label htmlFor="senha">Senha</label> 
-                    <input id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha} />                                      
-                  </div>
+                  <InputCustomizado id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome} label="Nome"/>
+                  <InputCustomizado id="email" type="text" name="email" value={this.state.email} onChange={this.setEmail} label="Email"/>
+                  <InputCustomizado id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha} label="Senha"/>
+                  
                   <div className="pure-control-group">                                  
                     <label></label> 
                     <button type="submit" className="pure-button pure-button-primary">Gravar</button>                                    
